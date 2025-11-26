fa3b7e4635f29b51a6380cd99a93179dbe73972f
Thejaswi Mudda <theju.mudda@gmail.com>
2025-11-26 16:21:39 +0530

    Create js\script.js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener(
+    "DOMContentLoaded", 
+    function(event) {
+        var message = document.getElementById('message');
+        message.innerText = "The script ran!";
+    }
+);
